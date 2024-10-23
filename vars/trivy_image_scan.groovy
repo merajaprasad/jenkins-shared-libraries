958a5ac7c6c2393a97201ad417ebc74fb766aa1a
Raja Prasad <113759499+merajaprasad@users.noreply.github.com>
2024-10-23 09:25:22 +0530

    Update trivy_image_scan.groovy

--- vars/trivy_image_scan.groovy
+++ vars/trivy_image_scan.groovy
@@ -1,3 +1,3 @@
-def call() {
-  sh "trivy image --format table -o image.html ${DOCKER_USR}/demoimage:latest"
+def call(String DockerUsr, String ProjectName, ImageTag) {
+  sh "trivy image --format table -o image.html ${DockerUsr}/${ProjectName}:${ImageTag}"
 }
