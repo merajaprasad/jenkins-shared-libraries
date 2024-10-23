3b8cbe59ab46f8f226afe35370e676d275d1b58f
Raja Prasad <113759499+merajaprasad@users.noreply.github.com>
2024-10-23 09:07:26 +0530

    Update sonarqube_code_analysis.groovy

--- vars/sonarqube_code_analysis.groovy
+++ vars/sonarqube_code_analysis.groovy
@@ -1,5 +1,5 @@
 def call(String SonarQubeServer, String ProjectName, String ProjectKey) {
   withSonarQubeEnv("${SonarQubeServer}") {
-      sh '''$SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=${ProjectName} -Dsonar.projectKey=${ProjectKey}'''
+      sh "$SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=${ProjectName} -Dsonar.projectKey=${ProjectKey}"
   }
 }
